1cc6c9103b443d2362415da37c156f432056455a
yash500071155 <75775134+yash500071155@users.noreply.github.com>
2021-05-26 11:11:46 +0530

    Update ocr.py

--- ocr.py
+++ ocr.py
@@ -1,9 +1,8 @@
 DATA_DIR = 'data/'
-DATASET = 'fashion-mnist' 
-TEST_DATA_FILENAME = DATA_DIR + DATASET + '/t10k-images-idx3-ubyte'
-TEST_LABELS_FILENAME = DATA_DIR + DATASET + '/t10k-labels-idx1-ubyte'
-TRAIN_DATA_FILENAME = DATA_DIR + DATASET + '/train-images-idx3-ubyte'
-TRAIN_LABELS_FILENAME = DATA_DIR + DATASET + '/train-labels-idx1-ubyte'
+TEST_DATA_FILENAME = DATA_DIR + '/t10k-images.idx3-ubyte'
+TEST_LABELS_FILENAME = DATA_DIR + '/t10k-labels.idx1-ubyte'
+TRAIN_DATA_FILENAME = DATA_DIR +  '/train-images.idx3-ubyte'
+TRAIN_LABELS_FILENAME = DATA_DIR +  '/train-labels.idx1-ubyte'
 
 
 def bytes_to_int(byte_data):
@@ -15,6 +14,8 @@ def read_images(filename, n_max_images=None):
     with open(filename, 'rb') as f:
         _ = f.read(4)  # magic number
         n_images = bytes_to_int(f.read(4))
+        if n_max_images:
+            n_images = n_max_images
         n_rows = bytes_to_int(f.read(4))
         n_columns = bytes_to_int(f.read(4))
         for image_idx in range(n_images):
@@ -65,7 +66,11 @@ def dist(x, y):
 def get_training_distances_for_test_sample(X_train, test_sample):
     return [dist(train_sample, test_sample) for train_sample in X_train]
 
-def knn(X_train, y_train, X_test, y_test, k=3):
+
+def get_most_frequent_element(l):
+    return max(l, key=l.count)
+
+def knn(X_train, y_train, X_test,y_test, k=3):
     y_pred = []
     for test_sample_idx, test_sample in enumerate(X_test):
         training_distances = get_training_distances_for_test_sample(X_train, test_sample)
@@ -77,14 +82,15 @@ def knn(X_train, y_train, X_test, y_test, k=3):
             )
         ]
         candidates = [
-            bytes_to_int(y_train[idx])
+            y_train[idx]
             for idx in sorted_distance_indices[:k]
         ]
-        print(f'point is{bytes_to_int(y_test[test_sample_idx])} and we guessed {candidates}')
+        print(f'point is {y_test[test_sample_idx]} and we guessed {candidates}')
         y_sample = 5
-        y_pred.append(y)
-    
+        top_candidate = get_most_frequent_element(candidates)
+        y_pred.append(top_candidate)
     return y_pred
+
 
 
 
@@ -92,18 +98,17 @@ def knn(X_train, y_train, X_test, y_test, k=3):
 def main():
     
     X_train = read_images(TRAIN_DATA_FILENAME, 1000)
-    y_train = read_labels(TRAIN_LABELS_FILENAME, n_train)
+    y_train = read_labels(TRAIN_LABELS_FILENAME)
     X_test = read_images(TEST_DATA_FILENAME, 5)
-    y_test = read_labels(TEST_LABELS_FILENAME)
-
-  
+    y_test = read_labels(TEST_LABELS_FILENAME, )
 
     X_train = extract_features(X_train)
     X_test = extract_features(X_test)
 
-     knn(X_train, y_train, X_test, k)
+    knn(X_train, y_train, X_test,y_test, 3)
 
-   
+    
+
 
 if __name__ == '__main__':
     main()
